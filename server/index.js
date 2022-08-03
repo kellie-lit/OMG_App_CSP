df85071f5d1c3737fe122666a24f09043ac2ca90
Kell Lit <107881418+kellie-lit@users.noreply.github.com>
2022-08-03 12:55:09 -0400

    OMG

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express')
+const path = require('path')
+ 
+const app = express()
+ 
+app.use(express.static(path.join(__dirname, "/../public")))
+ 
+app.get('/', function(req,res) {
+   res.sendFile(path.resolve('public/index.html'))
+})
+ 
+const port = process.env.PORT || 4005
+ 
+app.listen(port, () => {
+   console.log(`Listening on port ${port}`)
+})
